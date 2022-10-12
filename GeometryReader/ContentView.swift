338f361ee8a8c0d36f064aa9dc12292457bbf5d7
Jonni Åkesson <enterthesource@me.com>
2022-10-12 18:41:33 +0200

    Done

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { i in
+                    GeometryReader { geo in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(Angle(degrees: getProcentage(geo: geo) * 40),
+                                              axis: (x: 0, y: 1, z: 0))
+                        
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func getProcentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        
+        return Double(1 - (currentX / maxDistance))
     }
 }
 
