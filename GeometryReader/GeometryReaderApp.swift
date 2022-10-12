a299738b45e8abfd231396077666bac3fb60c0c6
Jonni Åkesson <enterthesource@me.com>
2022-10-12 18:18:38 +0200

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Jonni Akesson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
